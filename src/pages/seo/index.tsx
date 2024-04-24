f41ef55db54f5b4915421795153b47fac1cda906
Thandepzai <cuthan2k1@gmail.com>
2024-04-24 21:08:58 +0700

    add some

--- src/pages/seo/index.tsx
+++ src/pages/seo/index.tsx
@@ -1,3 +1,19 @@
-const Seo = () => <>dssđa</>;
+import Head from "next/head";
+
+const Seo = () => {
+  return (
+    <>
+      <Head>
+        <title>
+          Hướng Dẫn SEO Web Sử Dụng Meta Title và Meta Description Hiệu Quả
+        </title>
+        <meta
+          name="description"
+          content="Khám phá cách tối ưu hóa SEO cho website của bạn với hướng dẫn chi tiết sử dụng Meta Title và Meta Description để cải thiện thứ hạng tìm kiếm và thu hút lưu lượng truy cập nhiều hơn."
+        ></meta>
+      </Head>
+    </>
+  );
+};
 
 export default Seo;
